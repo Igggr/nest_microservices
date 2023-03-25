a5fbd335e5fc4176088e180580b70afdcbdf1d3b
Igor Smirnov <uchetzapis@yahoo.com>
2023-03-25 16:41:56 +0300

    Use ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,20 @@ import { UserModule } from './user/user.module';
 import { ProfileModule } from './profile/profile.module';
 import { Profile } from './profile/entitties/profile-entities';
 import { User } from './user/entities/user-entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: '123456',
-      database: 'test',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Profile],
       autoLoadEntities: true,
       synchronize: true,  // автоматические миграции
